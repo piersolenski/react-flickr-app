5915daa903eff1e7453b1ad9b885e1f446162cf6
Will Bryant <Will@Wills-MacBook-Pro.local>
2018-01-07 21:39:17 -0800

    Switched on service worker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 // Dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // Utilities
-import registerServiceWorker from './js/utils/registerServiceWorker';
+import registerServiceWorker from "./js/utils/registerServiceWorker";
 // Components
-import App from './js/App';
+import App from "./js/App";
 // eslint-disable-next-line
-import './css/app.scss';
+import "./css/app.scss";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
 
-// registerServiceWorker();
+registerServiceWorker();
